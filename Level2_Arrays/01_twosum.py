9d72e51bb7bc2f1f6f5695847d6c9391f0651713
Devarsh Raj <devarshraj5@gmail.com>
2026-01-29 15:49:36 +0530

    Implement two_sum function with target parameter for index retrieval

--- Level2_Arrays/01_twosum.py
+++ Level2_Arrays/01_twosum.py
@@ -2,9 +2,14 @@
 # return the indices of the two numbers such that they add up to target.
 # Assume that each input would have exactly one solution, and you may not use the same element twice.
 
-def two_sum_approach_1(arr : list):
+def two_sum_approach_1(arr : list, target):
     
-    return
+    seen = {}
+    for i, num in enumerate(arr):
+        complement = target - num
+        if complement in seen:
+            return seen[complement], i
+        seen[num] = i
 
 
 
